c1ce549fc6d308ecd45e7d473f76ec5cefe169f1
Mik <mik23@protonmail.ch>
2020-09-27 06:14:54 +0530

    changed Stats to functional components for hooks

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## [portfolio.kaijulabs.xyz](https://portfolio.kaijulabs.xyz/)
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/072157ba-23ff-48bf-8fc7-bedea1cd1920/deploy-status)](https://app.netlify.com/sites/compassionate-shirley-5e240d/deploys)
+[![Netlify Status](https://api.netlify.com/api/v1/badges/072157ba-23ff-48bf-8fc7-bedea1cd1920/deploy-status)](https://app.netlify.com/sites/mik/deploys)
 
 ## Celestrial Sun and Moon
 ### Used for theme button
--- components/Stats.js
+++ components/Stats.js
@@ -1,34 +1,34 @@
 import Tilt from "react-parallax-tilt";
+import { useMediaQuery } from "react-responsive";
 
-export default class Stats extends React.Component {
-  render() {
-    return (
-      <div className="main-div">
-        <Tilt gyroscope scale={1.5}>
-          <img
-            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=Mik1337&title_color=ff69b4&langs_count=10&layout=compact&theme=${
-              this.props.dark ? "dark" : "light"
-            }`}
-            alt="My top used programming, scripting or markup Languages"
-            className="skill"
-          />
-        </Tilt>
-        <style jsx>
-          {`
+export default function Stats(props) {
+  const isSmallScreen = useMediaQuery({ query: "(min-device-width: 600px)" });
+  return (
+    <div className="main-div">
+      <Tilt gyroscope scale={isSmallScreen ? 1.5 : 1}>
+        <img
+          src={`https://github-readme-stats.vercel.app/api/top-langs/?username=Mik1337&title_color=ff69b4&langs_count=10&layout=compact&theme=${
+            props.dark ? "dark" : "light"
+          }`}
+          alt="My top used programming, scripting or markup Languages"
+          className="skill"
+        />
+      </Tilt>
+      <style jsx>
+        {`
+          .skill {
+            padding: 20px;
+            height: 300px;
+            width: 420px;
+          }
+          @media (max-width: 600px) {
             .skill {
-              padding: 20px;
-              height: 300px;
-              width: 420px;
+              padding: 10px;
+              width: 90vw;
             }
-            @media (max-width: 600px) {
-              .skill {
-                padding: 10px;
-                width: 90vw;
-              }
-            }
-          `}
-        </style>
-      </div>
-    );
-  }
+          }
+        `}
+      </style>
+    </div>
+  );
 }
