c1347e12cd4c8dbce85248dcb6b6ba418d5adf50
Mik <mik23@protonmail.ch>
2020-09-27 07:28:36 +0530

    accidentlly commited my dyslescix blob

--- pages/index.js
+++ pages/index.js
@@ -99,10 +99,10 @@ export default class Index extends React.Component {
               I go by the alias of Mik online and Santosh irl, you can call me
               whichever. I'm <b Style="color: #33c748">Vegan</b>, I love cats,
               and I'm pretty good with a <code>*nix</code> computer.
-              I like working on new and interesting fields and growing my areas of
+              {/* I like working on new and interesting fields and growing my areas of
                 expertise. Design, Automation, Data Management and Web
                 Development are areas that I've worked in extensivly. I'm Currently,
-                working with The/Nudge helping them mapout SDG-16 for India.
+                working with The/Nudge helping them mapout SDG-16 for India. */}
             </p>
             <p>
               <b
