f3bfa41d9c78f380fd5cc3d817624b2cda49f604
Mik <mik23@protonmail.ch>
2020-10-15 11:48:22 +0530

    nyooom

--- components/Stats.js
+++ components/Stats.js
@@ -1,13 +1,94 @@
-import Tilt from "react-parallax-tilt";
-import { useMediaQuery } from "react-responsive";
+import PropTypes from "prop-types";
+import Tree from "react-tree-graph";
+
+// import { useMediaQuery } from "react-responsive";
+import useWindowDimensions from "./useWindowDimension";
 
 export default function Stats(props) {
-  const isSmallScreen = useMediaQuery({ query: "(min-device-width: 600px)" });
+  // const isSmallScreen = useMediaQuery({ query: "(min-device-width: 600px)" });
+  const { height, width } = useWindowDimensions();
+
+  const margins = { bottom: 100, left: 20, right: 150, top: 10 };
+  const data = {
+    name: "Mik",
+    textProps: { x: 0, y: -20 },
+    children: [
+      {
+        name: "Front End",
+        textProps: { x: 0, y: -20 },
+        children: [
+          {
+            name: "HTML / CSS",
+          },
+          {
+            name: "JavaScipt",
+          },
+          {
+            name: "ReactJS",
+          },
+          {
+            name: "VueJS",
+          },
+        ],
+      },
+      {
+        name: "Computer Languages",
+        textProps: { x: 0, y: -20 },
+        children: [
+          {
+            name: "Python",
+          },
+          {
+            name: "NodeJS",
+          },
+          {
+            name: "Shell",
+          },
+        ],
+      },
+      {
+        name: "DataBases",
+        textProps: { x: 0, y: -20 },
+        children: [
+          {
+            name: "PostgreSQL",
+          },
+          {
+            name: "MySQL",
+          },
+          {
+            name: "MongoDB",
+          },
+        ],
+      },
+      {
+        name: "Misc",
+        textProps: { x: 0, y: -20 },
+        children: [
+          {
+            name: "FMEA",
+          },
+          {
+            name: "Canva",
+          },
+          {
+            name: "GraphQL",
+          },
+          {
+            name: "JAMSTACK",
+          },
+        ],
+      },
+    ],
+  };
+
   return (
     <div className="main-div" className="title" id="skills" title="skills">
       <p>skills</p>{" "}
       <p className="description">
-        {isSmallScreen ? (
+        {console.log({ height, width })}
+        <Tree data={data} animated height={height} width={width - 20} />
+        {/* {isSmallScreen ? (
           <img
             src={`https://github-readme-stats.vercel.app/api/top-langs/?username=Mik1337&title_color=ff69b4&langs_count=10&layout=compact&theme=${
               props.dark ? "dark" : "light"
@@ -25,11 +106,10 @@ export default function Stats(props) {
               className="skill"
             />
           </Tilt>
-        )}
+        )} */}
         <style jsx>
           {`
             .skill {
-              // padding: 20px;
               height: 300px;
               width: 420px;
             }
